19fd786b7788542faf165989b1bf8b769f066a10
allXgaming <mrperfectguide.in@gmail.com>
2026-02-22 10:41:28 +0530

    Add main.js to import API and security modules
    This file imports various API and security modules to ensure they are loaded and executed.

---
+++ js/z-integration/main.js
@@ -0,0 +1,9 @@
+// main.js – imports all API and security modules
+import '../api/cloudinary.js';
+import '../api/firebase.js';
+import '../api/jikan.js';
+import '../api/utils.js';
+import '../security/logic.js';
+import '../security/rule.js';
+
+// No additional logic needed – these imports ensure all modules are loaded and executed.
